0341589ab08ec2746e60f06bf6a545037e9045fa
amywinehousing <amywinehousing@protonmail.com>
2025-12-13 11:10:05 +0000

    added a simple card check logic. added card remove feature

--- main.cpp
+++ main.cpp
@@ -82,6 +82,7 @@ int main() {
     int cardSuitAmount = 4;
     cout << "[?] Enter your desired deck size, it can be 24, 36, or 52: ";
     cin >> deckSize;
+    cout << endl;
     switch (deckSize) {
         case 24:
             cardValueAmount = 6;
@@ -107,16 +108,50 @@ int main() {
     }
     for (int i = 0; i < i + 1; i++) {
         int tempValue, tempSuit;
-        cout << "[?] Enter the card value (2-13, 0 to finish): ";
+        cout << "[?] Enter the card value (2 - 14, 11 - Jack, 12 - Queen, 13 - King, 14 - Ace, 0 to finish, to remove a card type it in again): ";
         cin >> tempValue;
         if (tempValue == 0) {
             cout << endl;
             break;
         }
         cout << endl;
-        cout << "[?] Enter the card suit (1-4): ";
+        cout << "[?] Enter the card suit (1 - Spades, 2 - Clubs, 3 - Hearts, 4 - Diamonds): ";
         cin >> tempSuit;
         cout << endl;
+        switch (cardValueAmount) {
+            case 6:
+                if (tempValue < 9 || tempValue > 14 || tempSuit < 1 || tempSuit > 4) {
+                    cout << "[-] Invalid card." << "\n\n";
+                }
+                if (cardArr[tempValue - 9][tempSuit - 1] == 1) {
+                    cout << "[!] Card removed." << "\n\n";
+                    cardArr[tempValue - 9][tempSuit - 1] = 0;
+                    continue;
+                }
+                break;
+            case 9:
+                if (tempValue < 9 || tempValue > 14 || tempSuit < 1 || tempSuit > 4) {
+                    cout << "[-] Invalid card." << "\n\n";
+                    continue;
+                }
+                if (cardArr[tempValue - 6][tempSuit - 1] == 1) {
+                    cout << "[!] Card removed." << "\n\n";
+                    cardArr[tempValue - 6][tempSuit - 1] = 0;
+                    continue;
+                }
+                break;
+            case 13:
+                if (tempValue < 9 || tempValue > 14 || tempSuit < 1 || tempSuit > 4) {
+                    cout << "[-] Invalid card." << "\n\n";
+                    continue;
+                }
+                if (cardArr[tempValue - 2][tempSuit - 1] == 1) {
+                    cout << "[!] Card removed." << "\n\n";
+                    cardArr[tempValue - 2][tempSuit - 1] = 0;
+                    continue;
+                }
+                break;
+        }
         switch (cardValueAmount) {
             case 6:
                 cardArr[tempValue - 9][tempSuit - 1] = 1;
